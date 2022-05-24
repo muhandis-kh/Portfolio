651620b1a040990059f51a0df2b42ce128ec9757
umarodev <ohojimirzayev@gmail.com>
2022-05-24 13:01:07 +0300

    Change bootstrap previos icons

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
 
-const navItem = document.querySelectorAll('.navbar-item');
-const navLink = document.querySelectorAll('.nav-link');
+// const navItem = document.querySelectorAll('.navbar-item');
+// const navLink = document.querySelectorAll('.nav-link');
 
-let a,b;
+// let a,b;
 
 
-navItem[a].addEventListener('click', function(){
-   navLink[a].classList.add('my-active');
-   navLink[b].classList.remove('my-active')
-})
+// navItem[a].addEventListener('click', function(){
+//    navLink[a].classList.add('my-active');
+//    navLink[b].classList.remove('my-active')
+// })
+
+let footerNavItem = document.querySelectorAll('.footer-nav__item')
+let footerNavLink = document.getElementsByClassName('footer-nav__item-link');
+
+
+console.log(footerNavItem);
