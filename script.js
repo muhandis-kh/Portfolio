9e9f25692480085ef1f7e837d982e2c657b98b1f
umarodev <ohojimirzayev@gmail.com>
2022-06-01 18:38:37 +0300

    Add script files

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 
 
-// const navItem = document.querySelectorAll('.navbar-item');
-// const navLink = document.querySelectorAll('.nav-link');
-
-// let a,b;
-
-
-// navItem[a].addEventListener('click', function(){
-//    navLink[a].classList.add('my-active');
-//    navLink[b].classList.remove('my-active')
-// })
-
-let footerNavItem = document.querySelectorAll('.footer-nav__item')
-let footerNavLink = document.getElementsByClassName('footer-nav__item-link');
+const navItems = document.querySelectorAll('.navbar-items');
+const navLink = document.querySelectorAll('.nav-link')
+let active = navLink[0];
+let acctive
+let k = 1;
 
 
-console.log(footerNavItem);
+navItems.forEach(function (item){
+   item.addEventListener('click', (e) => {
+      if(k === 1){
+         active.classList.remove('my-active');
+         e.target.classList.add('my-active');
+         acctive = e.target;
+         k = 0;
+      }
+      else{
+         acctive.classList.remove('my-active');
+         e.target.classList.add('my-active');
+         active = e.target;
+         k = 1
+      }
+
+   })
+})
+
+const footerNavItems = document.querySelectorAll('.footer-nav__item')
+console.log(footerNavItems)
+const footerNavLinks = document.querySelectorAll('.footer-nav__item-link')
+let j = 1;
+let footerActive = footerNavLinks[0];
+let emptyFooterActive;
+
+footerNavItems.forEach(function (item){
+   item.addEventListener('click', (e) =>{
+      if(j === 1){
+         footerActive.classList.remove('footer-my-active');
+         e.target.classList.add('footer-my-active');
+         emptyFooterActive = e.target;
+         j = 0;
+      }
+      else{
+         emptyFooterActive.classList.remove('footer-my-active');
+         e.target.classList.add('footer-my-active');
+         footerActive = e.target;
+         j = 1
+      }
+   })
+})
